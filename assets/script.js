bb39c998af9d29c6b746c36c078806dab5f75f14
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-06-18 21:45:18 -0400

    add select password options

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,54 @@
 // Assignment code here
+var characterLength;
+var selectLowercase;
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChars = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 
+// Function to select length of charcters for password
+function choosePassword() {
+  // Prompt to select how many characters to add
+  characterLength = prompt("Please choose how many characters you would like your password to be (Note: password length must be between 8 and 128 characters).");
+  // If statement for length = null
+  if (characterLength === null) {
+    passwordText.value = "Your Secure Password";
+  // else if statement for character choice must be more than 8 characters
+  }
+  if (characterLength < 8){
+    alert("Your password length must be more than 8 characters!");
+  // else if statement for character choice must be less than 128 characters
+  }
+  if (characterLength > 128){
+    alert("Your password length must be less than 128 characters!");
+  }
+  var selectLowercase = confirm("Please choose if you would like to include lowercase characters in your password. (Note: click Ok for Yes or Cancel for No).");
+  var selectUppercase = confirm("Please choose if you would like to include uppercase characters in your password. (Note: click Ok for Yes or Cancel for No).");
+  var selectNumbers = confirm("Please choose if you would like to include numbers in your password. (Note: click Ok for Yes or Cancel for No).");
+  var selectSpecial = confirm("Please choose if you would like to include special characters in your password. (Note: click Ok for Yes or Cancel for No).");
+  var passwordOptions = {
+    characterLength: characterLength,
+    selectLowercase: selectLowercase,
+    selectUppercase: selectUppercase,
+    selectNumbers: selectNumbers,
+    selectSpecial: selectSpecial
+  }
+  return passwordOptions;
+}
+
+// Function to select lowercase characters for password
+// Function to select uppercase characters for password
+
+function generatePassword() {
+  var options = choosePassword();
+  var finalPassword = [];
+  var charactersUsed = [];
+  var possibleCharacters = [];
+  if (options.selectLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowercaseChars);
+    charactersUsed.push(lowercaseChars);
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,7 +60,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
